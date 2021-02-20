d907305f8258235f5d6c020c9e32a50860d17622
Nicky Billovits <njbillov@umich.edu>
2021-02-19 22:12:07 -0500

    Trying to fix menu pulling issues on login.

--- query.py
+++ query.py
@@ -289,24 +289,24 @@ class Account(graphene.ObjectType):
         params = {'session': parent['session']}
         check_menus_query = '''MATCH (a:Account {session: $session})-[]-(m: Menu)
                      CALL {
-                        WITH m MATCH (m)-[]-(r:Recipe) RETURN COLLECT(r) as list
+                        WITH m MATCH (m)-[]-(r:Recipe) RETURN COLLECT(r) as recipe_list
                      }
-                     RETURN m, list'''
-        results: neo4j.Result = get_db().run(check_menus_query, parameters=params)
+                     RETURN m, recipe_list'''
+        results = get_db().run(check_menus_query, parameters=params)
         menus = []
         for record in results:
             menu = unpack(record.get('m'))
             # print(record.get('list'))
             recipes = [json.loads(recipe['json']) if recipe['json'] is not None else unpack(recipe) for recipe in
-                       record.get('list')]
+                       record.get('recipe_list')]
             # print(recipes)
             menu['recipes'] = recipes
             # print(menu)
             menus.append(menu)
 
-        # print(menus)
-        if len(menus) > 0:
-            return RequestMenu(ok=True, menus=menus)
+        print(menus)
+
+        return menus
 
     @staticmethod
     def resolve_mastered_skills(parent, info):
